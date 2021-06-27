c56d2e8fff4783306e85a86d2e1736d09a0ecebe
Mert Duzgun <duzgunnmert@gmail.com>
2021-06-27 14:44:57 +0300

    Add path aliases

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+
 module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@routes': path.resolve(__dirname, 'src/routes/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+    },
+  },
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
