d089d7f9c7b34b2fa03b3d73c8c162cc66a38b25
Mert Duzgun <duzgunnmert@gmail.com>
2021-06-27 23:37:30 +0300

    Refactor button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,10 +3,26 @@ import cx from 'classnames';
 
 import styles from './Button.module.scss';
 
-export default function Button({ className, theme }) {
+export default function Button({
+  className,
+  size = 'normal',
+  theme = 'primary',
+  disabled,
+  children,
+  ...otherProps
+}) {
   return (
-    <button className={cx(styles.base, styles[theme], className)}>
-      Button
+    <button
+      className={cx(
+        styles.base,
+        styles[theme],
+        { [styles[size]]: size !== 'normal' },
+        className
+      )}
+      disabled={disabled}
+      {...otherProps}
+    >
+      {children}
     </button>
   );
 }
