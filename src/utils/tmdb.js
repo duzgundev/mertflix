5565fe360531a086296ad4f41007462c7658556f
Mert Duzgun <duzgunnmert@gmail.com>
2021-07-25 21:54:10 +0300

    Get smaller images for homepage

--- src/utils/tmdb.js
+++ src/utils/tmdb.js
@@ -2,7 +2,7 @@
 // const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=`;
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-const IMG_API = `https://image.tmdb.org/t/p/original/`;
+const IMG_API = `https://image.tmdb.org/t/p/w400/`;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
