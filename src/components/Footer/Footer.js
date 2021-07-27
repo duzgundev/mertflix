776402ba65647f78c91380af8ffe41a922fe6e3f
Mert Duzgun <duzgunnmert@gmail.com>
2021-07-28 00:55:09 +0300

    Fill footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,20 @@
+import { HeartIcon } from '@heroicons/react/solid';
+
+import styles from './Footer.module.scss';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.base}>
+      <span>Made with&nbsp;</span>
+      <HeartIcon fill="red" width="24" />
+      <span>&nbsp;by&nbsp;</span>
+      <a
+        href="https://twitter.com/duxgun"
+        target="_blank"
+        className="text-gray-300 underline"
+      >
+        Mert Duzgun
+      </a>
+    </footer>
+  );
 }
