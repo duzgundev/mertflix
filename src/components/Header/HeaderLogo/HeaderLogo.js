bdd80eed8a9e0f024ed96c62f37900553fb6b5b4
Mert Duzgun <duzgunnmert@gmail.com>
2021-07-28 01:00:28 +0300

    Remove unnecessary stylesheet

--- src/components/Header/HeaderLogo/HeaderLogo.js
+++ src/components/Header/HeaderLogo/HeaderLogo.js
@@ -2,12 +2,10 @@ import Logo from '@images/logo.png';
 
 import { Link } from 'react-router-dom';
 
-import styles from './HeaderLogo.module.scss';
-
 export default function HeaderLogo() {
   return (
     <Link to="/">
-      <img className={styles.base} src={Logo} />
+      <img className="w-40" src={Logo} />
     </Link>
   );
 }
